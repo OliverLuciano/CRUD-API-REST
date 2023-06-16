819caa58a6aa103193e149675041ae93a2679086
José Luciano de Oliveira Filho <60415276+Romantictrash@users.noreply.github.com>
2023-06-16 17:29:52 -0300

    Banco de dados conectado ao Atlas

---
+++ server.js
@@ -0,0 +1,7 @@
+const port = process.env.PORT || 3000;
+import app from "./src/app.js";
+
+
+app.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`)
+})
---
+++ src/config/dbConnect.js
@@ -0,0 +1,7 @@
+import mongoose, { mongo } from "mongoose";
+
+mongoose.connect("mongodb+srv://lolciano:1234@cluster0.ao1l8cx.mongodb.net/");
+
+let db = mongoose.connection;
+
+export default db;
